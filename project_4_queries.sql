b48cf121c1310bd7d740f8b22a5355dfff8f084b
Yang Cline <yangcline@Yangs-MBP.knology.net>
2025-09-06 12:11:39 -0400

    timeSheet Data Insertion

--- project_4_queries.sql
+++ project_4_queries.sql
@@ -9,3 +9,9 @@ select * from `project_4`.`employee`;
 select * from `project_4`.`user`;
 
 select * from `project_4`.`time_off`;
+
+select YEARWEEK(DATE('2025-08-01'), 3);
+
+select * from `project_4`.`time_sheet`;
+
+ SELECT YEARWEEK('2025-09-06', 3);
