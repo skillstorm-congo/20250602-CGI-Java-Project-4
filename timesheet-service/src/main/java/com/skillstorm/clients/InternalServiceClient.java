b33bc75e02777e4c75467b0597aeb38ae7007e24
ttsosie <tiffiny.tsosie@mykenzie.snhu.edu>
2025-09-08 12:30:03 -0700

    After quick sessionw ith Jon

--- timesheet-service/src/main/java/com/skillstorm/clients/InternalServiceClient.java
+++ timesheet-service/src/main/java/com/skillstorm/clients/InternalServiceClient.java
@@ -4,11 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "internal-service")
+@FeignClient(name = "timesheet-service")
 public interface InternalServiceClient {
 	
 	//make sure you match the full URL suffix for the endpoint, the return type, and RequestParams/PathVariables/RequestBody
-	@GetMapping("/internal")
+	@GetMapping("/timesheet")
 	ResponseEntity<String> getHello();
 
 }
